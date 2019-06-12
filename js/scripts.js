75e852ed6ac27a8829e9489c1c5418a7244cdecd
Student <example@example.com>
2019-06-12 09:44:34 -0700

    Initial setup add html and scrips under Ben Martinson

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function checkYear(year){
+  // if(year%4 === 0)
+  //   return true
+  // else
+  //   return false
+  //same thing
+  return year%4 === 0;
+}
+
+console.log(checkYear(1904));
+
+$("document").ready(function(){
+  $(".inputYear").submit(function(event){
+    event.preventDefault();
+
+    var year = $("#inputOne").val();
+    var result = checkYear(year);
+
+    $(".result").show()
+    $("#year").text(year)
+    if(!result){
+      console.log("here")
+      $("#notAYear").text(" not ");
+    }
+    else{
+      $("#notAYear").hide();
+    }
+  })
+})
