3d72f5334b578f05ead1656c78a7b8f6a0aab175
Student <example@example.com>
2019-06-12 09:55:57 -0700

    add more logic sripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 function checkYear(year){
-  // if(year%4 === 0)
-  //   return true
-  // else
-  //   return false
-  //same thing
-  return year%4 === 0;
+  if(year%400 === 0)
+    return true
+  else if(year%100 ===0)
+    return false
+  else if (year%4 ===0)
+    return true
+  else
+    return false
+
+  // //same thing
+  //
+  // return year%4 === 0;
 }
 
 console.log(checkYear(1904));
